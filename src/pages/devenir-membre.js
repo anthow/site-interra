edc56b541737856fc320d6ab5d67372931694d65
anthow <anthonyenglebert@gmail.com>
2022-04-07 13:45:06 +0200

    /

--- src/pages/devenir-membre.js
+++ src/pages/devenir-membre.js
@@ -12,7 +12,7 @@ const devenirMembre = () => (
 
                         
             <iframe width="540" height="2305" title="formulaire devenir membre"
-            src="https://ba8902f1.sibforms.com/serve/MUIEAC4csB3ofNcxmjnf-ZcaBPYcanHUxET1UG-7PQC4Bif_x2vPfkv4g-1po5KIE6nk9ZnM1qmnPCHlQ4g8rMQIQRGgRLJJveqkA4hwvXaF8rQOiYt-35Z_qJyP9uygrJe4e_g5_E_aW7_fiQM31ak68Bv6NT8pn9elGYXWoBMdSkexVi11ozFQdZRvoSgNjlxvwoP98hFbIx5i_" 
+src="https://ba8902f1.sibforms.com/serve/MUIEALkmZmjVLKSxY8c1DJfmlNHIr09xXCW9zc7bEpb7Wor8TC_AhQlgWvG2GFHTjBLmbNtgt-YsTRBDNRgzf1HS28HUzeGCHikRJtWQqs27S6evPiuyqoWS433J8qXKn54-DOtxLxgaNjXJ4grFE0gQGJG60qGI_3tQ6D3xtctR6qpoylvjVP7BRXT3YGQbmHtVHhiDG89rGJ0u"
             frameborder="0" scrolling="auto" allowfullscreen styles="display: block;margin-left: auto;margin-right: auto;max-width: 100%;"></iframe>              </article>
         </div>
     </Layout>
