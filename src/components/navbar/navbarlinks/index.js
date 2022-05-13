8daf1c1f3ada427c258dd7809b54f01324aac0fe
anthow <anthonyenglebert@gmail.com>
2022-05-13 23:29:48 +0200

    Update index.js

--- src/components/navbar/navbarlinks/index.js
+++ src/components/navbar/navbarlinks/index.js
@@ -120,7 +120,7 @@ const NavbarLinks = () => {
           <li className="text-center mb-2">
             <div class="dropdown inline-block relative">
               <button class="  hover:text-yellow-500 text-center mb-2 md:mb-0   rounded inline-flex items-center">
-                <Link to="/a    gir-avec-nous">
+                <Link to="/agir-avec-nous">
                   Agir avec nous              </Link>
                 <svg class="fill-current h-4 w-4 mr-1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" /> </svg>
               </button>
