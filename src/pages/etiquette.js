1072d27b9e1068db4164325b6f1f40bec725ebb3
anthow <anthonyenglebert@gmail.com>
2022-06-08 12:34:10 +0200

    Update etiquette.js

--- src/pages/etiquette.js
+++ src/pages/etiquette.js
@@ -13,11 +13,11 @@ const etiquette = ({data}) => (
             <article className="w-10/12  mt-10   m-auto">
             <h1 className="font-black text-4xl mb-10  text-vert-interra  bg-white text-center"> {data.datoCmsAccueil.titreTiquette} </h1>
             <div className="flex flex-x-2 mb-20 w-12/12 justify-center m-auto">
-              {/*}   
+            
             <figure className=" ">
               <GatsbyImage image={data.datoCmsAccueil.imageEtiquetteUn.gatsbyImageData} alt={data.datoCmsAccueil.imageEtiquetteUn.alt} className="" />
 
-            </figure>
+            </figure>  {/*}   
             <figure className=" ">
               <GatsbyImage image={data.datoCmsAccueil.imageEtiquetteDeux.gatsbyImageData} alt={data.datoCmsAccueil.imageEtiquetteDeux.alt} className="" />
 
