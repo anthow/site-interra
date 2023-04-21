3c7d8af01b05b48928e69f440eaeae6df51be292
anthow <anthonyenglebert@gmail.com>
2023-04-21 10:13:56 +0200

    Update former-duo.js

--- src/pages/agir-avec-nous/former-duo.js
+++ src/pages/agir-avec-nous/former-duo.js
@@ -13,36 +13,41 @@ const devenirBenevole = ({ data }) => (
 
             <article className=" w-10/12 md:w-1/2  mt-10 border p-5 rounded border-orange-interra  m-auto">
 
-            <form action="https://formspree.io/f/mnqrrkoa" method="post" className="flex flex-col gap-5">
+            <form name="inscription duo"
+            method="post"
+            data-netlify="true"
+            data-netlify-honeypot="bot-field"  className="flex flex-col gap-5">
+                          <input type="hidden" name="form-name" value="inscription duo" />{" "}
+
     <div className="flex flex-col gap-2">
-      <label className="text-orange-interra font-bold text-lg">Nom</label>
+      <label name="Nom" htmlFor="Nom" className="text-orange-interra font-bold text-lg">Nom</label>
       <input type="text" name="nom" id="nom" className="border bg-gray-200 hover:bg-white focus:outline focus:outline-orange-interra " />  
     </div>
     <div className="flex flex-col gap-2">
-      <label className="text-orange-interra font-bold text-lg">Prénom</label>
+      <label name="prénom" htmlFor="prénom" className="text-orange-interra font-bold text-lg">Prénom</label>
       <input type="text"  name="prenom" id="prenom" className="border bg-gray-200 hover:bg-white focus:outline focus:outline-orange-interra " />  
     </div>
     <div className="flex flex-col gap-2">
-      <label className="text-orange-interra font-bold text-lg">Téléphone</label>
+      <label name="téléphone" htmlFor="téléphone" className="text-orange-interra font-bold text-lg">Téléphone</label>
       <input type="text"  name="telephone" id="telephone" className="border bg-gray-200 hover:bg-white focus:outline focus:outline-orange-interra " />  
     </div>
     <div className="flex flex-col  gap-2">
-      <label className="text-orange-interra font-bold text-lg">Mail</label>
+      <label name="mail" htmlFor="mail" className="text-orange-interra font-bold text-lg">Mail</label>
       <input type="mail"  name="mail" id="mail"  className="border bg-gray-200 hover:bg-white focus:outline focus:outline-orange-interra "/>  
     </div>
   
 <fieldset className="flex flex-col  gap-2">
-    <legend className=" mb-2 text-lg">Je suis:</legend>
+    <legend name="je suis" htmlFor="je suis " className=" mb-2 text-lg">Je suis:</legend>
 
     <div>
-      <input type="radio" id="primo-arrivante" name="jesuis" value="primo-arrivante" className="mr-2"
+      <input type="radio" id="primo-arrivante" name="jesuis" value="primo-arrivant(e)" className="mr-2"
              />
-      <label for="primo-arrivante">Une personne "nouvellement arrivée en Wallonie" (tu ne te sens pas toujours inclus.e dans la société, tu as besoin de mieux connaitre le français, tu as peu de réseau...)</label>
+      <label name="primo-arrivant(e)" htmlFor="primo-arrivant(e)" for="primo-arrivant(e)">Une personne "nouvellement arrivée en Wallonie" (tu ne te sens pas toujours inclus.e dans la société, tu as besoin de mieux connaitre le français, tu as peu de réseau...)</label>
     </div>
 
     <div className="">
       <input type="radio" id="locale" name="jesuis" value="locale" className="mr-2"/>
-      <label for="locale">Une personne "locale" (tu te sens inclus.e dans la société, tu as une bonne connaissance du français et tu disposes d'un réseau social suffisant)</label>
+      <label name="locale" htmlFor="locale"  for="locale">Une personne "locale" (tu te sens inclus.e dans la société, tu as une bonne connaissance du français et tu disposes d'un réseau social suffisant)</label>
     </div>
 </fieldset>
 <input type="submit" value="Envoyer" className=" text-white bg-vert-interra font-black
